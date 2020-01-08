297ccdfe1acc1953d9e9a58f81f2a1ec2520515d
harini1783 <harini1783@gmail.com>
2020-01-07 20:47:25 -0600

    first commit

---
+++ src/test/java/qaclickacademy/selenium.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class selenium {
+
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI() {
+		System.out.println("ElementsUI");
+	}
+	
+	
+}
